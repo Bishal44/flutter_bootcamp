125fba272bd077fc537c0a47d4a85988c0f65eeb
Bishal <bhattaraibishal704@gmail.com>
2019-07-23 21:19:47 +0545

    widget

--- mi_card/mi_card/lib/main.dart
+++ mi_card/mi_card/lib/main.dart
@@ -10,17 +10,47 @@ class MyApp extends StatelessWidget {
         home :Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Container(
-              margin: EdgeInsets.only(left: 20.0),
-              padding: EdgeInsets.fromLTRB(3.0, 2.0, 5.0, 4.0),
-              color: Colors.white,
-              child: Text("hello"),
-
-
+            child: Column(
+              
+              children: <Widget>[
+            Container(
+            margin: EdgeInsets.only(left: 20.0),
+            padding: EdgeInsets.fromLTRB(3.0, 2.0, 5.0, 4.0),
+            color: Colors.white,
+            child: Text("hello1"),
+            ),
+                
+                Container(
+            margin: EdgeInsets.only(left: 20.0),
+            padding: EdgeInsets.fromLTRB(3.0, 2.0, 5.0, 4.0),
+            color: Colors.white, child: Text("hello2"),
+            ), 
+                
+                SizedBox(
+                  height: 20.0,
+                  width: 20.0,
+                ), 
+                Container(
+                  margin: EdgeInsets.only(left: 20.0),
+                  padding: EdgeInsets.fromLTRB(3.0, 2.0, 5.0, 4.0),
+                  color: Colors.white, 
+                  child: Text("hello3"),
+                ),
+                Row(
+                  children: <Widget>[
+                    Image(
+                        image: NetworkImage("https://media.gettyimages.com/photos/starry-night-picture-id519760984?s=2048x2048")
+                    ),
+                    Image(
+                      image: NetworkImage("https://media.gettyimages.com/photos/portrait-of-beautiful-woman-without-makeup-picture-id641576958?s=2048x2048"),
+                    )
+                  ],
+                ),
+            ],
+            ),
             ),
           ),
-        )
-    );
+        );
 
   }
 
