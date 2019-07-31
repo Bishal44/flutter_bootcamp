535eaf2834fbc6d7203fca6380412f1ed408b6b9
Bishal <bhattaraibishal704@gmail.com>
2019-07-31 21:00:39 +0545

    changes

--- mi_card/mi_card/lib/main.dart
+++ mi_card/mi_card/lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
         home :Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Column(
+
+            child: Row(
               
               children: <Widget>[
             Container(
@@ -19,29 +20,51 @@ class MyApp extends StatelessWidget {
             color: Colors.white,
             child: Text("hello1"),
             ),
-                
                 Container(
-            margin: EdgeInsets.only(left: 20.0),
-            padding: EdgeInsets.fromLTRB(3.0, 2.0, 5.0, 4.0),
-            color: Colors.white, child: Text("hello2"),
+                  height: 20.0,
+                width: 50.0,
+                margin: EdgeInsets.only(left: 5.0),
+
+
+                  child: Text("hello2"),
             ), 
                 
                 SizedBox(
                   height: 20.0,
                   width: 20.0,
-                ), 
-                Container(
-                  margin: EdgeInsets.only(left: 20.0),
-                  padding: EdgeInsets.fromLTRB(3.0, 2.0, 5.0, 4.0),
-                  color: Colors.white, 
-                  child: Text("hello3"),
                 ),
-                Row(
+
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          margin: EdgeInsets.only(left: 20.0),
+                          padding: EdgeInsets.fromLTRB(3.0, 2.0, 5.0, 4.0),
+                          color: Colors.white,
+                          child: Text("hello3"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Column(
                   children: <Widget>[
                     Image(
+                        height: 200.0,
+                        width: 300.0,
+
                         image: NetworkImage("https://media.gettyimages.com/photos/starry-night-picture-id519760984?s=2048x2048")
+
+                    ),
+                    SizedBox(
+                      height: 2.0,
                     ),
                     Image(
+                      height: 200.0,
+                      width: 300.0,
                       image: NetworkImage("https://media.gettyimages.com/photos/portrait-of-beautiful-woman-without-makeup-picture-id641576958?s=2048x2048"),
                     )
                   ],
